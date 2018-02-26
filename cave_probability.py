586895ef6397d7990503586d792103c693026791
zojs <bandazasvojena97@gmail.com>
2018-02-26 21:18:05 +1400

    Case a

--- cave_probability.py
+++ cave_probability.py
@@ -1,5 +1,25 @@
 import random
 times_t=[1,2,3]
+already=set()
+times=0
+avg_sum=0
+for i in range(100000):
+    curtime=0
+    already=set()
+    while 1:
+        st=random.randint(0,2)
+        if (st not in already):
+             print(st)
+             curtime+=times_t[st];
+             already.add(st)
+             if st==0:
+                 break
+    print("out")
+    avg_sum+=curtime
+    times+=1
+print("a)",avg_sum/times)
+
+times_t=[1,2,3]
 times=0
 avg_sum=0
 for i in range(10000000):
@@ -11,4 +31,4 @@ for i in range(10000000):
             break
     avg_sum+=curtime
     times+=1
-print(avg_sum/times)
+print("b)",avg_sum/times)
