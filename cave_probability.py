cd69b52e1e5198aa93eb4f79f497f44b9d3675e6
zojs <bandazasvojena97@gmail.com>
2018-02-26 21:19:39 +1400

    Removed debugging, reduced count on b

--- cave_probability.py
+++ cave_probability.py
@@ -9,12 +9,12 @@ for i in range(100000):
     while 1:
         st=random.randint(0,2)
         if (st not in already):
-             print(st)
+             #print(st)
              curtime+=times_t[st];
              already.add(st)
              if st==0:
                  break
-    print("out")
+    #print("out")
     avg_sum+=curtime
     times+=1
 print("a)",avg_sum/times)
@@ -22,7 +22,7 @@ print("a)",avg_sum/times)
 times_t=[1,2,3]
 times=0
 avg_sum=0
-for i in range(10000000):
+for i in range(1000000):
     curtime=0
     while 1:
         st=random.randint(0,2)
